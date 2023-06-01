4d74923a3e652c8561f517c77f25d84104e145ae
Kaue Sousa <kauelimasousa02@gmail.com>
2023-05-31 22:55:12 -0300

    Payment, one-to-one association

--- src/main/java/com/estudandoweb/course/config/TestConfig.java
+++ src/main/java/com/estudandoweb/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudandoweb.course.entities.Category;
 import com.estudandoweb.course.entities.Order;
 import com.estudandoweb.course.entities.OrderItem;
+import com.estudandoweb.course.entities.Payment;
 import com.estudandoweb.course.entities.Product;
 import com.estudandoweb.course.entities.User;
 import com.estudandoweb.course.entities.enums.OrderStatus;
@@ -82,5 +83,8 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemsRepository.saveAll(Arrays.asList(oi1, oi2, oi3,oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
